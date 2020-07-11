97ffda151f1f1111c5b506035b0a589f133d7f9d
Sebastian <sebastian97-19@hotmail.com>
2020-07-10 22:49:18 -0500

    laying out the components of pugstagram.

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,3 +1,13 @@
+<script>
+    import Profile from './Profile.svelte';
+    import Stories from './Stories.svelte';
+    import Footer from './Footer.svelte';
+</script>
+
 <div class="Sidebar">
-    Sidebar
+    <div class="Sidebar-container">
+        <Profile />
+        <Stories />
+        <Footer />
+    </div>
 </div>
